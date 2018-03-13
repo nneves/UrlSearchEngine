b1c7ac5520f5b9dfd4f731d45ce5d2b2b4a0623d
Nelson Neves <nelson.neves@ott.local>
2018-03-13 22:54:27 +0000

    update startup script from UI, restore only node_modules if folder doesn't exist

--- UI/start.sh
+++ UI/start.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 echo "Run /start.sh script ...";
 
-echo "Restore node_modules back into /src folder!";
-mv /dev_node_modules /src/node_modules;
+if [ ! -d "/src/node_modules" ]; then
+    echo "Restore node_modules back into /src folder!";
+    mv /dev_node_modules /src/node_modules;
+fi
 
 echo "Run app: npm start";
 cd /src;
